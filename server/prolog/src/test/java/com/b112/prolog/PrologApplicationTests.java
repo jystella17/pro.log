18b15bc6150896f7d1a3b82c145ae2dc2676671d
Jeongyeon <jeongyeon.park216@gmail.com>
2024-01-31 10:40:34 +0900

    Initial Commit

---
+++ server/prolog/src/test/java/com/b112/prolog/PrologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b112.prolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
