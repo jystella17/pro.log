e8f9e9a46e002591383b7d3e67c32bac05695863
Jeongyeon <jeongyeon.park216@gmail.com>
2024-02-05 17:47:22 +0900

    [Add] Redis Subscribe setting

--- server/prolog/src/main/java/com/b112/prolog/chat/config/RedisConfig.java
+++ server/prolog/src/main/java/com/b112/prolog/chat/config/RedisConfig.java
@@ -1,12 +1,18 @@
 package com.b112.prolog.chat.config;
 
+import com.b112.prolog.chat.subscriber.RedisSubscriber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -25,12 +31,31 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer messageListenerContainer(MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(listenerAdapter, channelTopic());
+
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "onMessage");
+    }
+
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
     }
 }
---
+++ server/prolog/src/main/java/com/b112/prolog/chat/subscriber/RedisSubscriber.java
@@ -0,0 +1,46 @@
+package com.b112.prolog.chat.subscriber;
+
+import com.b112.prolog.chat.dto.ChattingDTO;
+import com.b112.prolog.chat.dto.SubMessageDTO;
+import com.b112.prolog.chat.exception.PubNotFoundException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.Random;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedisSubscriber implements MessageListener {
+
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final SimpMessageSendingOperations sendingOperations;
+
+    @Override
+    public void onMessage(@NonNull Message message, byte[] pattern) {
+        try {
+            String publish = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+            ChattingDTO pubMsg = objectMapper.readValue(publish, ChattingDTO.class);
+
+            // User Repository 연결 후 삭제
+            Random random = new Random();
+            String randInt = random.toString();
+
+            SubMessageDTO subMsg = new SubMessageDTO("사용자" + randInt , pubMsg.getMessage(), pubMsg.getCreatedAt());
+            sendingOperations.convertAndSend("/chat/sub/" + pubMsg.getRoomId(), subMsg);
+        } catch (Exception e) {
+            throw new PubNotFoundException(e.getMessage());
+        }
+    }
+}
