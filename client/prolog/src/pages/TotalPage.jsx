37c233b67f13b25af1a1e6ea4c1ea8ec8bbd82fd
Jiwon <yjw5602@naver.com>
2024-02-05 12:30:30 +0900

    [Add] router 코드 추가

--- client/prolog/src/pages/TotalPage.jsx
+++ client/prolog/src/pages/TotalPage.jsx
@@ -1,26 +1,41 @@
-import styled from 'styled-components'
-
-// import Sidebar from "../components/sidebar/Sidebar";
-import Side from "../components/sidebar/Side";
+import styled from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import MyMaster from "./MyMaster";
+import MasterPaper from "./MasterPaper";
+import MyPage from "./MyPage";
+import AllChatting from "./AllChatting";
+import MyChatting from "./MyChatting";
+import MainPage from "./MainPage";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const TotalPage = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function Result() {
-    return (
-        <TotalPage>
-            <Side />
-            <Content>
-                <div>header bar</div>
-                <div>여기에 화면을 출력하세요</div>
-            </Content>
-        </TotalPage>
-    )
+  return (
+    <BrowserRouter>
+      <TotalPage>
+        <Sidebar />
+        <Content>
+          <div>header bar</div>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/mymaster" element={<MyMaster />} />
+            <Route path="/masterpaper" element={<MasterPaper />} />
+            <Route path="/myPage" element={<MyPage />} />
+            <Route path="/allchatting" element={<AllChatting />} />
+            <Route path="/mychatting" element={<MyChatting />} />
+          </Routes>
+        </Content>
+      </TotalPage>
+    </BrowserRouter>
+  );
 }
