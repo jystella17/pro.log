823586cea56849e42d79699d67b4b7ad8bd62f70
Jiwon <yjw5602@naver.com>
2024-02-06 11:05:15 +0900

    [Add] masterPaper 추가 & 삭제 기능 추가

--- client/prolog/src/components/masterPaper/QnA.jsx
+++ client/prolog/src/components/masterPaper/QnA.jsx
@@ -1,56 +1,86 @@
 // 질문 답변 컴포넌트(질문, 답변-실시간 글자수)
-// 질문 답변 컴포넌트(삭제버튼)
 
 // 질문 답변 추가 버튼
 // 기능
 import { useState } from "react";
 import styled from "styled-components";
+import { IoClose, IoAddCircle, IoAddCircleOutline } from "react-icons/io5";
 import "./QnA.scss";
+import { Flex, Input, ConfigProvider } from "antd";
+
+// qna 컴포넌트에 맞춰서 높이를 변화시키고 싶은데 안됨
+// 사이드바 길이또한..
 
 const ContainerAll = styled.div`
-  padding: 70px 100px;
-  /* display: flex;
-  justify-content: center; */
+  padding: 70px 100px 30px 100px;
   width: 60vw;
-  height: 100vh;
+  /* height: 100vh; */
   background-color: #7c93c326;
   border-radius: 10px;
 `;
 
-function QnAComponent({ id, question, answer, onRemove, onQChange, onAChange }) {
+// 실시간 글자수 적용 textarea
+const { TextArea } = Input;
+const onChange = (e) => {
+  console.log("Change:", e.target.value);
+};
+
+function QnAComponent({
+  id,
+  question,
+  answer,
+  onRemove,
+  onQChange,
+  onAChange,
+  index,
+  isRemovable,
+}) {
   return (
-    <div>
+    <div className="qna">
       {/* 질문 */}
-      <div>
-        <input
-          className="question"
-          type="text"
-          placeholder="질문을 입력하세요."
-          value={question}
-          // 입력 변경 시 onChange
-          onChange={(e) => onQChange(id, e.target.value)}
-        />
-        {/* 첫 번째 질문-답변 쌍이 아니면 삭제 버튼을 표시 */}
-        {id !== 0 && <button onClick={() => onRemove(id)}>X</button>}
+      <div className="qna-header">
+        <div className="qna-number">
+          <p>{index + 1}.</p>
+          <input
+            className="question"
+            type="text"
+            placeholder="질문을 입력하세요."
+            value={question}
+            // 입력 변경 시 onChange
+            onChange={(e) => onQChange(id, e.target.value)}
+          />
+        </div>
+        {/* qna가 하나 남았을 때는 삭제 불가 */}
+        {isRemovable && (
+          <div className="x-button" onClick={() => onRemove(id, index)}>
+            <IoClose />
+          </div>
+        )}
       </div>
       {/* 답변 */}
-      <textarea
-        cols="4"
-        rows="8"
-        className="answer"
-        type="text"
-        placeholder="답변을 입력하세요."
-        value={answer}
-        // 입력 변경 시 onChange
-        onChange={(e) => onAChange(id, e.target.value)}
-      />
+      {/* padding.. */}
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimaryHover: "",
+            colorPrimary: "lightgrey",
+          },
+        }}
+      >
+        <TextArea
+          showCount
+          cols="4"
+          rows="8"
+          className="answer"
+          type="text"
+          placeholder="답변을 입력하세요."
+          value={answer}
+          // 입력 변경 시 onChange
+          onChange={(e) => onAChange(id, e.target.value)}
+        />
+      </ConfigProvider>{" "}
     </div>
   );
-}
-
-// QnA 컴포넌트 추가
-function AddButton({ onAdd }) {
-  return <button onClick={onAdd}>+</button>;
 }
 
 // 상태관리
@@ -64,8 +94,10 @@ function QnAContainer() {
   }
 
   // 특정 QnA 삭제 함수
-  function removeQnA(id) {
-    setQnAs(qnas.filter((q) => q.id !== id));
+  function removeQnA(id, index) {
+    if (window.confirm(`문항 ${index + 1}을 삭제하시겠습니까?`)) {
+      setQnAs(qnas.filter((q) => q.id !== id));
+    }
   }
 
   // 질문 내용이 변경될 때 호출되는 함수
@@ -82,21 +114,24 @@ function QnAContainer() {
     <>
       <ContainerAll>
         <div className="content">
-          <div className="qna">
-            {qnas.map((q) => (
+          <div className="qnas">
+            {/* react에서 list의 각 항목을 렌더링 할 때는, 각 항목에 고유한 key prop이 있어야함 */}
+            {qnas.map((q, index) => (
               <QnAComponent
                 key={q.id}
                 id={q.id}
+                index={index}
                 question={q.question}
                 answer={q.answer}
                 onRemove={removeQnA}
                 onQChange={handleQuestionChange}
                 onAChange={handleAnswerChange}
+                isRemovable={qnas.length > 1}
               />
             ))}
           </div>
-          <div className="deleteButton">
-            <AddButton onAdd={AddQnA} />
+          <div className="addButton">
+            <IoAddCircleOutline size="60" color="#5D5D8A" onClick={AddQnA} />
           </div>
         </div>
       </ContainerAll>
--- client/prolog/src/pages/MasterPaper.jsx
+++ client/prolog/src/pages/MasterPaper.jsx
@@ -8,6 +8,8 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  /* border: 1px solid #7c93c326;
+  border-radius: 10px; */
 `;
 
 function MasterPaper() {
