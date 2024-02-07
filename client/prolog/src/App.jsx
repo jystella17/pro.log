e4532df421ded7e18a8b3389481fa2fc63f9c5e5
Jiwon <yjw5602@naver.com>
2024-02-07 09:34:50 +0900

    [Feat] template-Memo 생성

--- client/prolog/src/App.jsx
+++ client/prolog/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import TotalPage from "./pages/TotalPage";
+import Memo from "./components/templates/memo/Memo";
 
 function App() {
-  return <TotalPage />;
+  // return <TotalPage />;
+  return <Memo />;
 }
 
 export default App;
