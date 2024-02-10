34a4c9aabddb7631d4dbfa66c2f725ba08b5d300
Sanghun <ph38230119@gmail.com>
2024-02-10 17:51:52 +0900

    [Feat] Custom 예외 처리 구현
    요청의 예외를 분류합니다.
    인증이 되지 않았을 때는 NotAuthenticatedException, UserNotFoundException이 발생할 수 있고, 401 코드를 응답합니다.
    인가가 되지 않았을 때는 AccessDeniedException이 발생할 수 있고, 403 코드를 응답합니다.

--- server/prolog/src/main/java/com/b112/prolog/user/exception/UserNotFoundException.java
+++ server/prolog/src/main/java/com/b112/prolog/user/exception/UserNotFoundException.java
@@ -3,6 +3,9 @@ package com.b112.prolog.user.exception;
 import org.springframework.dao.DataAccessException;
 
 public class UserNotFoundException extends DataAccessException {
+    public UserNotFoundException() {
+        super(ExceptionMessage.USER_NOT_FOUND.getMessage());
+    }
 
     public UserNotFoundException(String msg) {
         super(msg);
