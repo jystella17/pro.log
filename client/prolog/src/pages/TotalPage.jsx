b9cf3806e995e4389f5a82a52e0f85a2a93a6770
Jiwon <yjw5602@naver.com>
2024-02-12 14:53:44 +0900

    [Refactor] kakao login  코드 수정

--- client/prolog/src/pages/TotalPage.jsx
+++ client/prolog/src/pages/TotalPage.jsx
@@ -2,24 +2,22 @@ import styled from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import MasterPaper from "./MasterPaper";
-import MyMaster from "./MyMaster"
+import MyMaster from "./MyMaster";
 import MyInfo from "./MyInfo";
 import Chatting from "./Chatting";
 import MainPage from "./MainPage";
 import Sidebar from "../components/sidebar/Sidebar";
 import Headbar from "../components/sidebar/Headbar";
-import NoProcess from "./NoProcess"
+import NoProcess from "./NoProcess";
 import YesProcess from "./YesProcess";
-import CT from "../components/templates/ct/CT"
+import CT from "../components/templates/ct/CT";
 import Login from "./Login";
 import WebRtc from "./WebRtc";
 
 const AppContainer = styled.div`
   width: 1519px;
   height: 737px;
-  
 `;
-
 
 const TotalPageContent = styled.div`
   // width: 1536px;
@@ -35,21 +33,20 @@ export default function Result() {
         <Sidebar />
         <Headbar />
         <TotalPageContent>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/masterpaper" element={<MasterPaper />} />
-              <Route path="/myinfo" element={<MyInfo />} />
-              <Route path="/chatting" element={<Chatting />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/process" element={<NoProcess />} />
-              <Route path="/process/:selectedJdId" element={<YesProcess />} />
-              <Route path="/process/:company" element={<NoProcess />} />
-              <Route path="/webrtc" element={<WebRtc />} />
-            </Routes>
-          </TotalPageContent>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/masterpaper" element={<MasterPaper />} />
+            <Route path="/myinfo" element={<MyInfo />} />
+            <Route path="/chatting" element={<Chatting />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/process" element={<NoProcess />} />
+            <Route path="/process/:selectedJdId" element={<YesProcess />} />
+            <Route path="/process/:company" element={<NoProcess />} />
+            <Route path="/webrtc" element={<WebRtc />} />
+          </Routes>
+        </TotalPageContent>
       </AppContainer>
     </BrowserRouter>
   );
 }
-
