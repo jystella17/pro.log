32ecb31cfd023e2b799859fd279bdaf903cd7ec0
Sanghun <ph38230119@gmail.com>
2024-02-13 14:41:11 +0900

    [Refactor] User Entity 수정
    wishCompany, processes, developer, newbie, qnas 필드 추가

--- server/prolog/src/main/java/com/b112/prolog/user/entity/User.java
+++ server/prolog/src/main/java/com/b112/prolog/user/entity/User.java
@@ -1,21 +1,46 @@
 package com.b112.prolog.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
+import com.b112.prolog.process.entity.Process;
+import com.b112.prolog.process.entity.Qna;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
 
 @Document(collection = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Getter
 @Builder
-
 public class User {
     @Id
     private String id; // OAuth Provider가 제공한 사용자 고유 식별 id
     private String email;
     private String nickname;
+    private List<String> wishCompany;
+    @DocumentReference
+    private List<Process> processes;
+    private boolean developer;
+    private boolean newbie;
+    @DocumentReference
+    private List<Qna> qnas;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", wishCompany=" + wishCompany +
+                ", processes=" + processes +
+                ", developer=" + developer +
+                ", newbie=" + newbie +
+                ", qnas=" + qnas +
+                '}';
+    }
 }
