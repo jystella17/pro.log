1468a5dbf6b23afaf69f385859921a492a65400f
박예원 <parkyw0321@gmail.com>
2024-02-13 12:38:36 +0900

    [Refactor] rename packagename

---
+++ server/prolog/src/main/java/com/b112/prolog/jobdescription/dto/CompanyDto.java
@@ -0,0 +1,11 @@
+package com.b112.prolog.jobdescription.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class CompanyDto {
+    private Long companyId;
+    private String companyName;
+}
---
+++ server/prolog/src/main/java/com/b112/prolog/process/repository/QnaRepository.java
@@ -0,0 +1,9 @@
+package com.b112.prolog.process.repository;
+
+import com.b112.prolog.process.entity.Qna;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface QnaRepository extends MongoRepository<Qna, String>, CustomRepository {
+
+}
