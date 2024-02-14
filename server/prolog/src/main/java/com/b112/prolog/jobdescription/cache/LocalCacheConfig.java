113fb60e1fb805a8a5e26eda1a60ec238f0b5cd4
Jeongyeon <jeongyeon.park216@gmail.com>
2024-02-14 13:49:32 +0900

    [Fix] Local Cache 사용을 위한 설정

---
+++ server/prolog/src/main/java/com/b112/prolog/jobdescription/cache/LocalCacheConfig.java
@@ -0,0 +1,23 @@
+package com.b112.prolog.jobdescription.cache;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class LocalCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(List.of(new ConcurrentMapCache("job-description")));
+
+        return cacheManager;
+    }
+}
