9826fe2b84ab2f4beade98dc93f2f205258c6c58
박예원 <parkyw0321@gmail.com>
2024-02-14 09:51:59 +0900

    [Style] 폰트 적용

--- client/prolog/src/App.jsx
+++ client/prolog/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from 'recoil'
 import TotalPage from './pages/TotalPage'
 import './App.css'
+import './assets/fonts.css'
 
 import styled from 'styled-components'
 // const Page = styled.div`
