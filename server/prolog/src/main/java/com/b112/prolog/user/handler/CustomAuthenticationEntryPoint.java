950b7f6583e26ea7a4f8e5f5a7ce8ff9ebf02b38
Sanghun <ph38230119@gmail.com>
2024-02-16 02:30:19 +0900

    [Fix] Exception Stack Trace 출력

--- server/prolog/src/main/java/com/b112/prolog/user/handler/CustomAuthenticationEntryPoint.java
+++ server/prolog/src/main/java/com/b112/prolog/user/handler/CustomAuthenticationEntryPoint.java
@@ -1,12 +1,12 @@
 package com.b112.prolog.user.handler;
 
-import java.io.IOException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Component;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -14,6 +14,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
             throws IOException {
-        response.sendError(401, exception.getMessage());
+        exception.fillInStackTrace();
+        response.sendError(401, exception.getLocalizedMessage());
     }
 }
