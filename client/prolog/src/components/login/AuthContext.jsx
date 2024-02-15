eefa5676702d20a21fbf09d83f952bd9eb0f4d8d
Jiwon <yjw5602@naver.com>
2024-02-16 03:40:10 +0900

    [Add] sidebar 로그인 분기 추가

--- client/prolog/src/components/login/AuthContext.jsx
+++ client/prolog/src/components/login/AuthContext.jsx
@@ -1,5 +1,6 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import Cookies from "js-cookie";
+import { setCookie, getCookie, removeCookie } from "../../cookie/cookie";
 
 const AuthContext = createContext();
 
@@ -9,11 +10,18 @@ export function useAuth() {
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    const token = Cookies.get("access_token");
-    setIsLoggedIn(!!token);
+    const fetchedToken = getCookie("access_token");
+    setToken(fetchedToken);
   }, []);
+
+  useEffect(() => {
+    // const token = Cookies.get("access_token");
+
+    setIsLoggedIn(!!token);
+  }, [token]);
 
   return <AuthContext.Provider value={{ isLoggedIn }}>{children}</AuthContext.Provider>;
 };
