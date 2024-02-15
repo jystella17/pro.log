1206c6f5621781e28c8b2ce9cb993a58f66f9f93
Sanghun <ph38230119@gmail.com>
2024-02-15 22:40:59 +0900

    [Fix] User master qna list null check

--- server/prolog/src/main/java/com/b112/prolog/user/exception/ExceptionMessage.java
+++ server/prolog/src/main/java/com/b112/prolog/user/exception/ExceptionMessage.java
@@ -11,7 +11,7 @@ public enum ExceptionMessage {
     LOGOUT_FAILED("Logout Failed"),
     ACCESS_TOKEN_EXPIRED("Access Token Expired"),
     REFRESH_TOKEN_EXPIRED("Refresh Token Expired"),
-    ACCESS_DENIED("Access Denied");
-    
+    ACCESS_DENIED("Access Denied"),
+    DATA_NOT_FOUND("Data Not Found");
     private final String message;
 }
